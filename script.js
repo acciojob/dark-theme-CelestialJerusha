9180da9b399fb12660d7290c4a1fee44384eda74
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-08-21 21:17:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+var btn = document.getElementById("swap");
 
+
+btn.addEventListener("click",swap);
+
+function swap(){
+    var div = document.getElementById("app");
+    div.classList.add("night");
+    btn.classList.add("button_night");
+
+}

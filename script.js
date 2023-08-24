c965b237fc94e77daa9dbe8c5d7f9071b27e3d23
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-08-24 20:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your code here
 
 function swapTheme(){
-	var btn = document.getElementById("swap");
+	var button = document.getElementById("swap");
     var main = document.getElementById("app");
-    main.classList.add("night");
-    btn.classList.add("button_night");
+    main.classList.toggle("night");
+    button.classList.toggle("button_night");
 
 }
